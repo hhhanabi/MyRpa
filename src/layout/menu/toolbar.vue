8ec6580019b295b3225e28ae1d1d7c6e5fcdcc83
hanabi <645482284@qq.com>
2023-12-28 13:11:20 +0800

    toolbar dialog

--- src/layout/menu/toolbar.vue
+++ src/layout/menu/toolbar.vue
@@ -1,33 +1,31 @@
 <template>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" />
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" id="el-tree"/>
   </template>
   
   <script lang="ts" setup>
+import Node from 'element-plus/es/components/cascader-panel/src/node.mjs';
+
+
   interface Tree {
     label: string
     children?: Tree[]
   }
   
-  const handleNodeClick = (data: Tree) => {
-    console.log(data)
+  const handleNodeClick = (data: Tree,node:Node) => {
+      if(!node.isLeaf) return
   }
   
   const data: Tree[] = [
     {
-      label: 'Level one 1',
+      label: '条件判断',
       children: [
         {
           label: 'Level two 1-1',
-          children: [
-            {
-              label: 'Level three 1-1-1',
-            },
-          ],
-        },
+        }
       ],
     },
     {
-      label: 'Level one 2',
+      label: '循环',
       children: [
         {
           label: 'Level two 2-1',
@@ -48,7 +46,7 @@
       ],
     },
     {
-      label: 'Level one 3',
+      label: '等待',
       children: [
         {
           label: 'Level two 3-1',
@@ -68,6 +66,18 @@
         },
       ],
     },
+    {
+      label:'网页自动化'
+    },
+    {
+      label:'Excel'
+    },
+    {
+      label:'数据处理'
+    },
+    {
+      label:'操作系统'
+    }
   ]
   
   const defaultProps = {
@@ -76,3 +86,8 @@
   }
   </script>
   
+<style lang="scss" scoped>
+#el-tree {
+  height: 100%;
+}
+</style>
